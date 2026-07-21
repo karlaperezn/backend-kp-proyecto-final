7ddd13f01828a58a7058bbce1f9e7152dbc7e39d
karlaperezn <karla.perezn96@alumnostrazos.net>
2026-07-21 19:51:18 +0200

    ep para cargar web a editar y ep para la web final

--- routes/weddings.js
+++ routes/weddings.js
@@ -7,7 +7,7 @@ router.get('/my-weddings/:userId', async (req, res) => {
     const userId = req.params.userId
 
     const weddingsByMe = await req.app.locals.db.collection('weddings').find({ ownerId: new ObjectId(userId) }).toArray();
-    const weddingsOwned = weddingsByMe.map(weddingByMe => ({...weddingByMe, role: 'admin'}))
+    const weddingsOwned = weddingsByMe.map(weddingByMe => ({ ...weddingByMe, role: 'admin' }))
 
     //Collabs
     const collabs = await req.app.locals.db.collection('collabs').find({ userId: new ObjectId(userId) }).toArray()
@@ -26,9 +26,10 @@ router.get('/my-weddings/:userId', async (req, res) => {
             role: collab.role
         }
     })
-    res.send({ weddingsOwned, weddingCollabs})
+    res.send({ weddingsOwned, weddingCollabs })
 })
 
+//crear web rsvp
 router.post('/new-wedding', async (req, res) => {
     const {
         ownerId,
@@ -75,7 +76,7 @@ router.post('/new-wedding', async (req, res) => {
 })
 
 
-//web invitados
+//mostrar web rsvp
 router.get('/:weddingSlug', async (req, res) => {
     const weddingSlug = req.params.weddingSlug;
 
@@ -92,8 +93,75 @@ router.get('/:weddingSlug', async (req, res) => {
         message = "Esta web no existe"
     }
 
-    res.send({wedding, status, message})
+    res.send({ wedding, status, message })
 
+})
+
+//cargar web para editar
+router.get('/preview/:weddingId', async (req, res) => {
+    const weddingId = req.params.weddingId;
+
+    let status;
+    let message;
+    let wedding;
+
+    try {
+        wedding = await req.app.locals.db.collection('weddings').findOne({ _id: new ObjectId(weddingId) });
+        status = true;
+        message = `Boda de ${wedding.brideName} y ${wedding.groomName}`
+    } catch (error) {
+        status = false;
+        message = "Esta web no existe"
+    }
+
+    res.send({ wedding, status, message })
+
+})
+
+//editar web rsvp
+router.put('/editar-boda/:weddingId', async (req, res) => {
+    const weddingId = req.params.weddingId;
+    const { userId, brideName, groomName, eventDate, ceremony, reception, design } = req.body;
+    console.log('userId recibido:', userId);
+
+    let status;
+    let message;
+    let weddingUpdated;
+
+    const wedding = await req.app.locals.db.collection('weddings').findOne({ _id: new ObjectId(weddingId) });
+
+    if (!wedding || wedding.ownerId.toString() !== userId) {
+        status = false;
+        message = 'No tienes permiso para editar esta boda';
+
+    } else {
+        try {
+            weddingUpdated = await req.app.locals.db.collection('weddings').updateOne(
+                { _id: new ObjectId(weddingId) },
+                {
+                    $set: {
+                        slug: `${brideName}-y-${groomName}`.toLowerCase().replace(/\s+/g, '-'),
+                        brideName,
+                        groomName,
+                        eventDate,
+                        ceremony,
+                        reception,
+                        design,
+                        updatedAt: new Date()
+                    }
+                }
+            );
+
+            status = true;
+            message = 'Web RSVP actualizada';
+
+        } catch (error) {
+            status = false;
+            message = 'Error al actualizar la web RSVP';
+        }
+    }
+
+    res.send({ data: weddingUpdated, status, message });
 })
 
 export default router;
